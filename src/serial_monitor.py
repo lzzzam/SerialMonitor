fe969aa6cd78f4ca398d265d5bd56e053c4ff1f7
lzzzam <58335995+lzzzam@users.noreply.github.com>
2022-01-22 21:49:35 +0100

    Fixed bug

--- src/serial_monitor.py
+++ src/serial_monitor.py
@@ -1,9 +1,12 @@
 from tkinter import *
 from tkinter import scrolledtext
 from tkinter.ttk import *
+import sys
+import serial
 import serial.tools.list_ports
-import comm
-from comm import ser, serialPlotter, transmitt, lock_serial, lock_thread, lock_window
+
+ser = serial.Serial(timeout=1)
+closeSerial = False
 
 window = Tk()
 window.title("Serial Monitor")
@@ -12,6 +15,24 @@ window.grid_columnconfigure(1, weight=1)
 window.grid_columnconfigure(2, weight=1)
 window.grid_columnconfigure(3, weight=1)
 window.grid_rowconfigure(1, weight=1)
+
+def serialHandler():
+    if(ser.is_open == True and ser.in_waiting > 0):
+        chr = ser.read_until()
+        monitor.config(state=NORMAL)
+        monitor.insert(END, chr)
+        if autoscroll_state.get() == True:
+            monitor.see("end")
+        monitor.config(state=DISABLED)
+        
+    if(closeSerial == False):       
+        window.after(1, serialHandler)
+    else:
+        if (ser.is_open == True):
+            ser.flush
+            ser.flushInput()
+            ser.close()
+        window.destroy()   
 
 def clearOutput():
     monitor.config(state=NORMAL)
@@ -26,14 +47,13 @@ def updatePort():
     port_sel["values"] = list
 
 def selectPort(self):
-    lock_serial.acquire()
+    global ser
     if(ser.is_open == True):
         ser.close()
         
     port=port_sel.get()
-    ser.port = port
     try:
-        ser.open()
+        ser = serial.Serial(port, timeout=1)
     except serial.SerialException as e:
         print(f"{e}")
         monitor.config(state=NORMAL)
@@ -41,15 +61,18 @@ def selectPort(self):
         if autoscroll_state.get() == True:
             monitor.see("end")
         monitor.config(state=DISABLED)
-    lock_serial.release()
         
-
+def transmitt(entry=None):
+    if(ser.is_open == True):
+        str = entry.get()
+        if str != "":
+            ser.write(str.encode('ascii'))
+            
 def closeSerialMonitor():
-    lock_window.acquire()  
-    comm.stop_serial_thread = True
-    lock_window.release()
-    lock_thread.acquire()
-    window.destroy()
+    global closeSerial
+    closeSerial = True
+    print(f"closeSerial = {closeSerial}")
+
 
 input = Entry(window)
 input.grid(row=0, column=0, columnspan=4, padx=1, pady=1, sticky=EW)
@@ -72,12 +95,10 @@ port_sel.grid(row=2, column=3, sticky=E)
 monitor = scrolledtext.ScrolledText(window,state=DISABLED, wrap=WORD)
 monitor.grid(row=1, column=0, columnspan=5, sticky=NSEW)
 
-Serial = serialPlotter("Serial Thread", monitor, autoscroll_state)
-Serial.start()
-
 window.wm_protocol("WM_DELETE_WINDOW", closeSerialMonitor)
-
 window.update()
+    
+window.after(1, serialHandler)
 
 sw = window.winfo_screenwidth()
 sh = window.winfo_screenheight()
